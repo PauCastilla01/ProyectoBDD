48e8139517a09300406906b687b7f25e9f26c869
PauCastilla01 <paulinacastillapadilla@comunidad.unam.mx>
2025-06-07 10:13:20 -0600

    Correccion de errores

--- s-01-netmax-usuario.sql
+++ s-01-netmax-usuario.sql
@@ -2,16 +2,13 @@
 --@Fecha creación: dd/mm/yyyy
 --@Descripción: Eliminación y creación del usuario.
 
-connect sys/system1 as sysdba;
-
 Prompt Creando al usuario netmax_bdd
+show con_name;
 drop user if exists netmax_bdd;
-
-
 
 create user netmax_bdd identified by netmax_bdd  quota unlimited on users;
 
 prompt Concediendo privilegios al usuario netmax_bdd
 grant create session, create table, create procedure, create sequence,
-    create database link, create synonym, create type, create view to editorial_bdd;
+    create database link, create synonym, create type, create view to netmax_bdd;
 
