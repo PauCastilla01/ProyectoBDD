d07ae0c097372b5d1d066bdd13e1b375c1c06f78
PauCastilla01 <paulinacastillapadilla@comunidad.unam.mx>
2025-06-07 21:46:16 -0600

    Funciones Blob 2

--- s-05-netmax-funciones-blob.sql
+++ s-05-netmax-funciones-blob.sql
@@ -57,31 +57,6 @@ end;
 /
 show errors
 
-create or replace function get_remote_archivo_f1_by_id(v_id in number)
-    return blob is
-    pragma autonomous_transaction;
-    v_temp_archivo blob;
-begin
---asegura que no haya registros
-    delete from ts_archivo_programa_1;
-    --inserta los datos obtenidos del fragmento remoto a la tabla temporal.
-    insert into ts_archivo_programa_1
-        select num_archivo, programa_id, archivo, tamanio from archivo_programa_f1 where num_archivo = v_id;
---obtiene el registro de la tabla temporal y lo regresa como blob
-    select archivo into v_temp_archivo
-    from ts_archivo_programa_1 where num_archivo = v_id;
---elimina los registros de la tabla temporal una vez que han sido obtenidos.
-    delete from ts_archivo_programa_1;
-    commit;
-    return v_temp_archivo;
-    exception
-        when others then
-        rollback;
-        raise;
-end;
-/
-show errors
-
 --Función que obtiene BLOB del fragmento 4
 create or replace function get_remote_trailer_f3_by_id(v_id in number)
     return blob is
@@ -127,6 +102,62 @@ begin
     return v_temp_archivo;
     exception
         when others then
+        rollback;
+        raise;
+end;
+/
+show errors
+
+--Función que obtiene BLOB del fragmento 1
+
+create or replace function get_remote_archivo_f1_by_id(
+    v_num_archivo in number,
+    v_programa_id in number
+) return blob is
+    pragma autonomous_transaction;
+    v_temp_archivo blob;
+begin
+    delete from ts_archivo_programa_1;
+    insert into ts_archivo_programa_1
+        select num_archivo, programa_id, archivo
+        from archivo_programa_f1
+        where num_archivo = v_num_archivo and programa_id = v_programa_id;
+    select archivo into v_temp_archivo
+    from ts_archivo_programa_1
+    where num_archivo = v_num_archivo and programa_id = v_programa_id;
+    delete from ts_archivo_programa_1;
+    commit;
+    return v_temp_archivo;
+exception
+    when others then
+        rollback;
+        raise;
+end;
+/
+show errors
+
+--Función que obtiene BLOB del fragmento 2
+
+create or replace function get_remote_archivo_f2_by_id(
+    v_num_archivo in number,
+    v_programa_id in number
+) return blob is
+    pragma autonomous_transaction;
+    v_temp_archivo blob;
+begin
+    delete from ts_archivo_programa_2;
+    insert into ts_archivo_programa_2
+        select num_archivo, programa_id, archivo
+        from archivo_programa_f1
+        where num_archivo = v_num_archivo and programa_id = v_programa_id;
+    select archivo into v_temp_archivo
+    from ts_archivo_programa_2
+    where num_archivo = v_num_archivo and programa_id = v_programa_id;
+    delete from ts_archivo_programa_2;
+    commit;
+    return v_temp_archivo;
+exception
+    when others then
         rollback;
         raise;
 end;
